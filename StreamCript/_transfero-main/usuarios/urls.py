403ee9a72f35ed527158e66a4a2ba73ae165562b
PedCruz18 <Pedroemanueldasilvacruz@gmail.com>
2025-05-28 16:40:39 -0300

    V0.0.2

---
+++ StreamCript/_transfero-main/usuarios/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from usuarios import views
+
+urlpatterns = [
+    path('cadastrar/', views.criarUsuario, name='criarusuario'),
+    path('login/', views.login, name='login'),
+]
+
+# 127.0.0.1:8000/usuario
+# 127.0.0.1:8000/usuario/login
+# 127.0.0.1:8000/usuario/cadastro
